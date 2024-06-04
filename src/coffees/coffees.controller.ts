2b0ca883b8650080cc8994a85398fb3b025054f2
javibueno21 <javier@javibueno.dev>
2024-06-04 11:27:21 +0200

    first commit: checking on nest course

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 @Controller('coffees')
 export class CoffeesController {
@@ -13,5 +21,10 @@ export class CoffeesController {
   get1Cafesitonomas(@Param('id') id: string): string {
     const cafesitos = ['cafe solo', 'cafe con lexe', 'cafe bonbom'];
     return cafesitos[id];
+  }
+
+  @Post()
+  crerate(@Body() body) {
+    return body;
   }
 }
